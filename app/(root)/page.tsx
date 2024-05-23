48b29a9401e390ffebdf1eb1778d00fe0fdce621
Yared Kebede <135129440+Yareda21@users.noreply.github.com>
2024-05-22 22:25:43 -0700

    second commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,30 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/totalBalanceBox'
 import React from 'react'
 
-const page = () => {
+const Home = () => {
+  const loggedIn = {
+    firstName: "Yared"
+  }
   return (
-    <div>page</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox 
+            type ="greeting"
+            title = "Welcome"
+            user = {loggedIn?.firstName || 'Guest'}
+            subtext = "Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox 
+            accounts= {[]}
+            totalBank={1}
+            totalCurentBalance={124.45}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
-export default page
+export default Home
---
+++ components/TotalBalanceBox.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TotalBalanceBox = ({
+    accounts=[], totalBanks, totalCurrentBalance,
+
+}) => {
+  return (
+    <div>totalBalanceBox</div>
+  )
+}
+
+export default TotalBalanceBox
